0502a79720d1d9a321c2e20dbc0220662b5d23b4
Christian Burger <christian@fivesqrd.com>
2017-09-24 16:39:02 +0200

    Moved atlas to its own repo

--- examples/.atlas/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-    'canvas' => [
-        'namespace' => 'Application\\Model', /* Specify the root namespace for model classes, i.e. Application\\Model */
-        'path'      => realpath(__DIR__) . '/../Application/Model' /* Specify the path where model PHP files should be created i.e. module/Application/src/Model */
-    ]
-);
